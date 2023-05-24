b660c20b88e3c1cfc4b8f7319945cfe32564bfcd
Asaju Enitan Michael <asajuenitan@gmail.com>
2023-05-24 09:30:13 +0100

    add nvim-tree overrides

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+local autocmd = vim.api.nvim_create_autocmd
 local opt = vim.opt
 
 opt.textwidth = 80
@@ -6,4 +7,16 @@ opt.showcmd = false
 opt.wrap = false
 opt.titlestring = " ❐ %t %r %m - %(%{expand('%:~:.:h\\')}%)%(\\ %a%)"
 opt.title = true
--- opt.completeopt = { 'noselect', 'noinsert', 'menu' }
+opt.completeopt = { 'noselect', 'noinsert', 'menu' }
+
+vim.cmd [[
+  hi LspReferenceWrite guibg=bold cterm=bold guifg=bold
+  hi LspReferenceText guibg=bold cterm=bold guifg=bold
+  hi LspReferenceRead guibg=bold cterm=bold guifg=bold
+]]
+
+-- auto resize when resizing nvim window
+autocmd('VimResized', {
+  pattern = '*',
+  command = 'tabdo wincmd =',
+})
--- plugins/base.lua
+++ plugins/base.lua
@@ -183,4 +183,20 @@ return {
       }
     end,
   },
+  {
+    'nvim-tree/nvim-tree.lua',
+    opts = {
+      git = {
+        enable = true,
+      },
+      renderer = {
+        highlight_git = true,
+        icons = {
+          show = {
+            git = true,
+          },
+        },
+      },
+    },
+  },
 }
